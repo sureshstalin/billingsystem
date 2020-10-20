dfa31f20ae77528df11ca8ddd62648fe23dc285f
Suresh Stalin <ssueresh.saikishore@gmail.com>
2020-10-20 11:21:44 +0530

    changed basedto to userdto in user registration

--- src/main/java/com/itgarden/controller/TestController.java
+++ src/main/java/com/itgarden/controller/TestController.java
@@ -3,6 +3,9 @@ package com.itgarden.controller;
 import com.itgarden.dto.UserDTO;
 import com.itgarden.entity.User;
 import com.itgarden.mapper.UserMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,20 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 
+@Slf4j
 @RestController
 @RequestMapping("api/public/test")
 public class TestController {
 
+//    Logger log = LoggerFactory.getLogger(this.getClass());
     @PostMapping("/mapper")
     public ResponseEntity<Void> mapperTesting(@RequestBody UserDTO userDto)  {
         User user = UserMapper.INSTANCE.userDTOtoUser(userDto);
-        System.out.println("Entity Class......");
-        System.out.println("Entity First Name " + user.getFirstName());
-        System.out.println("Entity Last Name " + user.getLastName());
+        log.debug("Entity Class......");
+        log.info("Entity First Name " + user.getFirstName());
+        log.info("Entity Last Name " + user.getLastName());
         UserDTO userDto2 = UserMapper.INSTANCE.userToUserDTO(user);
-        System.out.println("DTO Class......");
-        System.out.println("DTO First Name " + userDto2.getFirstName());
-        System.out.println("DTO Last Name " + userDto2.getLastName());
+        log.debug("DTO Class......");
+        log.info("DTO First Name " + userDto2.getFirstName());
+        log.info("DTO Last Name " + userDto2.getLastName());
+        log.trace("This is test trace message");
         return new ResponseEntity<Void>(HttpStatus.OK);
     }
 }
